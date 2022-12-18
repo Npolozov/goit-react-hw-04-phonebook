c201d68904c71d38dfd6bcb4608b2b0d2924af53
Nikita Polozov <npolozov0@gmail.com>
2022-12-18 20:55:46 +0200

    1

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,57 +1,43 @@
-import { Component } from 'react';
 import { Formik, Form, ErrorMessage } from 'formik';
 import { Wrapper, Button, Label, Input } from './Form.style';
 import * as yup from 'yup';
-
-const state = {
-  name: '',
-  number: '',
-};
 
 const schema = yup.object().shape({
   name: yup.string().required(),
   number: yup.number().required(),
 });
 
-export class ContactForm extends Component {
-  handleSubmit = (values, { resetForm }) => {
-    console.log(values);
-    this.props.onSubmit(values);
+export const ContactForm = ({ onSubmit }) => {
+  const state = {
+    name: '',
+    number: '',
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    onSubmit(values);
     resetForm();
   };
 
-  // handleChange = evt => {
-  //   const { name, value } = evt.target;
-  //   this.setState({ [name]: value });
-  // };
+  return (
+    <Formik
+      initialValues={state}
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <Wrapper>
+          <Label htmlFor="name">Name</Label>
+          <Input type="text" id="name" name="name" />
+          <ErrorMessage name="name" />
+        </Wrapper>
+        <Wrapper>
+          <Label htmlFor="number">Number</Label>
+          <Input id="number" type="tel" name="number" />
+          <ErrorMessage name="number" />
+        </Wrapper>
 
-  // reset = () => {
-  //   this.setState({ name: '', number: '' });
-  // };
-
-  render() {
-    return (
-      <Formik
-        initialValues={state}
-        validationSchema={schema}
-        onSubmit={this.handleSubmit}
-        // onChange={this.handleChange}
-      >
-        <Form>
-          <Wrapper>
-            <Label htmlFor="name">Name</Label>
-            <Input type="text" id="name" name="name" />
-            <ErrorMessage name="name" />
-          </Wrapper>
-          <Wrapper>
-            <Label htmlFor="number">Number</Label>
-            <Input id="number" type="tel" name="number" />
-            <ErrorMessage name="number" />
-          </Wrapper>
-
-          <Button type="submit">Add Contacs</Button>
-        </Form>
-      </Formik>
-    );
-  }
-}
+        <Button type="submit">Add Contacs</Button>
+      </Form>
+    </Formik>
+  );
+};
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,36 +1,33 @@
 import { Modal, BackDrop } from './Modal.styled';
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
+import { useEffect } from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class OpenModal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeydown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeydown);
-  }
+export const OpenModal = ({ onClose, children }) => {
+  useEffect(() => {
+    const handleKeydown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeydown);
 
-  handleKeydown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, [onClose]);
 
-  handleBackdrop = event => {
-    console.log(event.currentTarget);
-    console.log(event.target);
+  const handleBackdrop = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    return createPortal(
-      <BackDrop onClick={this.handleBackdrop}>
-        <Modal>{this.props.children}</Modal>
-      </BackDrop>,
-      modalRoot
-    );
-  }
-}
+
+  return createPortal(
+    <BackDrop onClick={handleBackdrop}>
+      <Modal>{children}</Modal>
+    </BackDrop>,
+    modalRoot
+  );
+};
